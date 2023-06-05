3ce6eb6122c0de26b619b5c8daf230f186396bbc
UlugbekMannonov <ulugbek.sbk.2021@gmail.com>
2023-06-05 20:50:44 +0800

    feat: develop Product controller logic

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -89,6 +89,21 @@ restaurantController.logout = (req, res) => {
     res.send("logout sahifasidasiz");
 };
 
+
+restaurantController.validateAuthRestaurant = (req, res, next) => {
+    if(req.session?.member?.mb_type === "RESTAURANT") {
+        req.member = req.session.member;
+        next();
+    } else 
+    res.json({
+        state: "fail", 
+        message: "only authenticated members with restaurant type",
+    });
+
+}
+ 
+
+
 restaurantController.checkSessions = (req, res) => {
     if(req.session?.member) {
         res.json({state: 'succed', data: req.session.member})
