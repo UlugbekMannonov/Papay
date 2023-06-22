010fed2bfae9326999fc27a0a7ef325efebedc2d
UlugbekMannonov <ulugbek.sbk.2021@gmail.com>
2023-06-22 19:24:48 +0800

    fix: fixing minor mistakes

--- controllers/productController.js
+++ controllers/productController.js
@@ -5,49 +5,53 @@ const Definer = require("../lib/mistake");
 let productController = module.exports;
 
 productController.getAllProducts = async (req, res) => {
-    try{
-        console.log("Get: cont/getAllProducts");
-    } catch(err) {
-        console.log(`ERROR, cont/getAllProducts, ${err.message}`);
-        res.json({state: 'fail', message: err.message });
-    }
+  try {
+    console.log("GET: cont/getAllProducts");
+  } catch (err) {
+    console.log(`ERROR, cont/getAllProducts, ${err.message}`);
+    res.json({ state: "fail", message: err.message });
+  }
 };
 
-
 productController.addNewProduct = async (req, res) => {
-    try{
-        console.log("POST: cont/addNewProduct");
-        assert(req.files, Definer.general_err3);
+  try {
+    console.log("POST: cont/addNewProduct");
+    assert(req.files, Definer.general_err3);
 
-        const product = new Product();
-        let data = req.body;
+    const product = new Product();
+    let data = req.body;
 
-        data.product_images = req.files.map((ele) => {
-            return ele.path;
-        });
+    data.product_images = req.files.map((ele) => {
+      return ele.path;
+    });
 
-        const result = await product.addNewProductData(data, req.member);
-
-        const html = `<script>
-                        alert('new product added successfully');
-                        window.location.replace('/resto/products/menu');
-                    </script>`;
-        res.end(html);
-
-    }catch(err) {
-        console.log(`ERROR, cont/addNewProduct, ${err.message}`);
-    }
+    const result = await product.addNewProductData(data, req.member);
+    const html = `<script>
+                     alert('new product added successfully');
+                     window.location.replace('/resto/products/menu')
+                  </script>`;
+    res.end(html);
+  } catch (err) {
+    console.log(`ERROR, cont/addNewProduct, ${err.message}`);
+  }
 };
 
 productController.updateChosenProduct = async (req, res) => {
-    try{
-        console.log("POST: cont/updateChosenProduct");
-        const product = new Product();
-        const id = req.params.id;
-        const result = await product.updateChosenProductData(id, req.body, req.member._id);
-        await res.json({state: "success", data: result});
-    } catch(err) {
-        console.log(`ERROR, cont/updateChosenProduct, ${err.message}`);
-        res.json({state: 'fail', message: err.message });
-    }
+  try {
+    console.log("POST: cont/updateChosenProduct");
+    const product = new Product();
+    const id = req.params.id;
+    // console.log("req===========", req);
+
+    const result = await product.updateChosenProductData(
+      id,
+      req.body,
+      req.member._id
+    );
+
+    await res.json({ state: "success", data: result });
+  } catch (err) {
+    console.log(`ERROR, cont/updateChosenProduct, ${err.message}`);
+    res.json({ state: "fail", message: err.message });
+  }
 };
