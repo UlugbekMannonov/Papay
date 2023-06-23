7ac138d99ba54ea7c622a8809044eef456957e2b
UlugbekMannonov <ulugbek.sbk.2021@gmail.com>
2023-06-24 00:26:00 +0800

    fix: complete develoment of allrestaurants business logic

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -168,5 +168,4 @@ restaurantController.updateRestaurantByAdmin = async (req, res) => {
     res.json({ state: "fail", message: err.message });
 
   }
-
 };
--- models/Restaurant.js
+++ models/Restaurant.js
@@ -22,30 +22,25 @@ class Restaurant {
         }
     }
 
-    async updateRestaurantByAdminData(update_data) {
-        try{
-            const id = shapeIntoMongooseObjectId(update_data?.id);
-            const result = await this.memberModel
-                .findByIdAndUpdate({ _id: id }, update_data, 
-                    {runValidators: true, 
-                    lean: true, 
-                    returnDocument: "after",
-                })
-            .exec(); 
+      async updateRestaurantByAdminData(update_data) {
+    try {
+      const id = shapeIntoMongosObjectId(update_data?.id);
+      const result = await this.memberModel
+        .findByIdAndUpdate({ _id: id }, update_data, {
+          runValidators: true,
+          lean: true,
+          returnDocument: "after",
+        })
+        .exec();
 
-            assert.ok(result, Definer.general_err1);
-            return result;
-        } catch (err) {
-            throw err;
-
-        }
+      assert.ok(result, Definer.general_err1);
+      return result;
+    } catch (err) {
+      throw err;
     }
-
-
-
-
-
+  }
 }
+
 
 
 
