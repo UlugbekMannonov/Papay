e12871639a175579e386e9b8a510eed07d71a54c
UlugbekMannonov <ulugbek.sbk.2021@gmail.com>
2023-07-15 16:18:32 +0800

    fix: checkMyAuthentication and retrieve token object data

--- controllers/memberController.js
+++ controllers/memberController.js
@@ -51,7 +51,7 @@ memberController.login = async (req, res) => {
 };
 
 memberController.logout = (req, res) => {
-    console.log("GET cont.logout");
+    console.log("GET cont/logout");
     res.send("logout sahifasidasiz");
 };
 
@@ -75,3 +75,19 @@ memberController.createToken = (result) => {
       throw err;
     }
   };
+
+  memberController.checkMyAuthentication = (req, res) => {
+    try {
+      console.log("GET cont/checkMyAuthentication");
+      let token = req.cookies["access_token"];
+      console.log("token:::", token);
+  
+      const member = token ? jwt.verify(token, process.env.SECRET_TOKEN) : null;
+      assert.ok(member, Definer.auth_err2);
+  
+      res.json({ state: "succeed", data: member });
+    } catch (err) {
+      throw err;
+    }
+  };
+  
