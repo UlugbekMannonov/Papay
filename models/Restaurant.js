03941fc586557c3dec88d52de2dd265cbee90264
UlugbekMannonov <ulugbek.sbk.2021@gmail.com>
2023-07-21 19:04:49 +0800

    feat: develop getChosenRestaurant API business logic

--- models/Restaurant.js
+++ models/Restaurant.js
@@ -2,6 +2,7 @@ const assert = require("assert");
 const MemberModel = require("../schema/member.model");
 const Definer = require("../lib/mistake");
 const { shapeIntoMongooseObjectId } = require("../lib/config");
+const Member = require("./Member.js");
 
 
 class Restaurant {
@@ -40,6 +41,29 @@ class Restaurant {
 
       const result = await this.memberModel.aggregate(aggregationQuery).exec();
       assert.ok(result, Definer.general_err1);
+      return result;
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  async getChosenRestaurantData(member, id) {
+    try {
+      id = shapeIntoMongooseObjectId(id);
+
+      if (member) {
+        const member_obj = new Member();
+        await member_obj.viewChosenItemByMember(member, id, "member");
+      }
+
+      const result = await this.memberModel
+        .findOne({
+          _id: id,
+          mb_status: "ACTIVE",
+        })
+        .exec();
+      assert.ok(result, Definer.general_err2);
+
       return result;
     } catch (err) {
       throw err;
