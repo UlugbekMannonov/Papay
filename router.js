c2895c8b5b2a0d8b1dac5c20f61c6a3fce41bfc6
UlugbekMannonov <ulugbek.sbk.2021@gmail.com>
2023-07-26 19:08:19 +0800

    feat: develop getMemberArticles rest api business logic

--- router.js
+++ router.js
@@ -82,4 +82,10 @@ router.post(
   communityController.createArticle
 );
 
+router.get(
+  "/community/articles",
+  memberController.retrieveAuthMember,
+  communityController.getMemberArticles
+);
+
 module.exports = router;
