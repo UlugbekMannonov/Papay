a87723e041e2378d23e11c480ec4772820b1049a
UlugbekMannonov <ulugbek.sbk.2021@gmail.com>
2023-07-28 20:36:07 +0800

    feat: develop business logic of member subscription

---
+++ schema/follow.model.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const { order_status_enums } = require("../lib/config");
+const Schema = mongoose.Schema;
+
+const followSchema = new mongoose.Schema(
+  {
+    follow_id: { type: Schema.Types.ObjectId, required: true },
+    subscriber_id: { type: Schema.Types.ObjectId, required: true },
+  },
+  { timestamps: true }
+);
+
+followSchema.index({ follow_id: 1, subscriber_id: 1 }, { unique: true });
+
+module.exports = mongoose.model("Follow", followSchema);
